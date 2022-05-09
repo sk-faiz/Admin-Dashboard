970da14c820e2b86a82b46d6bde4730c342bb4ba
Faiz Shaikh <77228339+sk-faiz@users.noreply.github.com>
2022-05-09 12:55:25 +0530

    First Commit

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './home.scss'
+
+function Home() {
+    return (
+        <div className="home">
+            <h1 className="title">
+                Admin
+            </h1>
+        </div>
+    )
+}
+
+export default Home
