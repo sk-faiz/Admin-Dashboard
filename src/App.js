bbf034e1d44f1d2b91a719f6f134d233d4cfbbdf
Faiz Shaikh <77228339+sk-faiz@users.noreply.github.com>
2022-05-09 13:16:55 +0530

    Setting up

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import Home from './pages/home/Home'
+import Login from './pages/login/Login'
+import Users from './pages/list/List'
+
+import {
+  BrowserRouter,
+  Switch,
+  Route,
+  Link,
+  Routes
+} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Home/>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/users' element={<Users />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
